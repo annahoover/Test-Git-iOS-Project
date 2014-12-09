cd6e94649bdcf8957a9bb332e7cbe938da794458
Anna Hoover <AnnaHoover@Annas-MacBook-Pro-2.local>
2014-12-09 13:23:40 -0500

    added a test class

---
+++ Git Test/GTTest.h
@@ -0,0 +1,13 @@
+//
+//  GTTest.h
+//  Git Test
+//
+//  Created by Anna Hoover on 12/9/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GTTest : NSObject
+
+@end
---
+++ Git Test/GTTest.m
@@ -0,0 +1,13 @@
+//
+//  GTTest.m
+//  Git Test
+//
+//  Created by Anna Hoover on 12/9/14.
+//
+//
+
+#import "GTTest.h"
+
+@implementation GTTest
+
+@end
